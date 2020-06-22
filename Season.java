07589e0e21055bb6a35817babeaa7d2579f8a9d9
hazellobo18 <38395317+hazellobo18@users.noreply.github.com>
2020-06-22 11:46:20 +0000

    Add files via upload

---
+++ Season.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class Season{
+public static void main(String[] args) {
+    Scanner s= new Scanner(System.in);
+    int var= s.nextInt();
+    
+    switch(var){
+        case 1: case 2: case 12:
+            System.out.println("WINTER");
+            break;
+
+        case 3: case 4: case 5:
+            System.out.println("Spring");
+            break;
+        
+        case 6: case 7: case 8:
+            System.out.println("Summer");
+            break;
+        
+        case 9: case 10: case 11:
+            System.out.println("Autumn");
+            break;
+
+        default:
+            System.out.println("No Such Season exists");
+            break;
+    }
+}
+    
+}
